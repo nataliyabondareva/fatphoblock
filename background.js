7c5e8eb76a79ede96a7ee48b0560cc14e7823d9b
Nataliya Bondareva <nataliya@MacBook-Pro-van-Nataliya.local>
2021-08-28 20:27:31 +0200

    Basic structure

--- background.js
+++ background.js
@@ -1,14 +1,34 @@
 let enabled = true;
-let blockList = ['oga'];
+let blockList = ["oga"];
+let linksToBlock = [];
 
-function findAndHide () {
-    document.querySelectorAll('*').forEach(node => {
-        node.childNodes.forEach(child => {
-          if (child.nodeType === 3) {
-            child.nodeValue.includes('oga') ? console.log(child.nodeValue) : null
-         }
-        });
-      })
+function replaceInnerHtml(node) {
+  console.log("child2", node);
+  // if (node.id = 'contents') {
+  // node.style.display = "none"
+  // node.remove()
+  node.innerHTML = "";
+  // }
+}
+
+function recursiveFindAndHide(node) {
+  if (node && node.id !== "dismissable") {
+    replaceInnerHtml(node);
+    return recursiveFindAndHide(node.parentNode);
+  } else if (node && node.id === "dismissable") {
+    return replaceInnerHtml(node);
+  }
+}
+
+function findAndHide() {
+  document.querySelectorAll("*").forEach((node) => {
+    node.childNodes.forEach((child) => {
+      if (child.nodeValue.includes("oga")) {
+        replaceInnerHtml(child);
+        return recursiveFindAndHide(child.parentNode);
+      }
+    });
+  });
 }
 
 window.onload = findAndHide();
